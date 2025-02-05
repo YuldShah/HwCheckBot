fa4067f84681e7744b1fcebab00c79070c92375c
YuldShah <notyuldshah@gmail.com>
2025-02-05 19:52:15 +0500

    adsfads

--- utils/db/storage.py
+++ utils/db/storage.py
@@ -1,47 +1,52 @@
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
+import sqlite3 as lite
 
 
-class DatabaseManager:
-    def __init__(self, uri, db_name):
-        """Connects to MongoDB using the given URI and database name."""
-        self.client = MongoClient(uri, server_api=ServerApi('1'))
-        self.db = self.client[db_name]
-        
-        # Ping the server to check the connection
-        try:
-            self.client.admin.command('ping')
-            print("[MongoDB] Successfully Connected!")
-        except Exception as e:
-            print(f"[MongoDB] Connection Error: {e}")
+class DatabaseManager(object):
 
-    def insert_one(self, collection, data):
-        """Insert a single document into a collection."""
-        return self.db[collection].insert_one(data).inserted_id
+    def __init__(self, path):
+        self.conn = lite.connect(path)
+        self.conn.execute('pragma foreign_keys = on')
+        self.conn.commit()
+        self.cur = self.conn.cursor()
 
-    def fetch_one(self, collection, query):
-        """Fetch a single document based on a query."""
-        return self.db[collection].find_one(query)
 
-    def fetch_all(self, collection, query={}):
-        """Fetch all documents matching a query. Defaults to all documents."""
-        return list(self.db[collection].find(query))
 
-    def update_one(self, collection, query, new_values):
-        """Update a single document."""
-        return self.db[collection].update_one(query, {"$set": new_values})
+    def create_tables(self):
+        self.query('CREATE TABLE IF NOT EXISTS users (idx INTEGER PRIMARY KEY, userid TEXT, fullname TEXT, username TEXT, regdate TEXT DEFAULT CURRENT_TIMESTAMP)')
+        self.query("CREATE TABLE IF NOT EXISTS folders (idx INTEGER PRIMARY KEY, title TEXT)")
+        self.query("CREATE TABLE IF NOT EXISTS messages (idx INTEGER PRIMARY KEY, jid INTEGER, msgid TEXT DEFAULT NULL, status INTEGER DEFAULT 0, date TEXT)")
+        self.query("CREATE TABLE IF NOT EXISTS exams (idx INTEGER PRIMARY KEY, code TEXT, title TEXT, about TEXT DEFAULT NULL, num_questions INTEGER, correct TEXT, folder INTEGER DEFUALT NULL, sdate TEXT DEFAULT NULL, duration INTEGER DEFAULT NULL, running INTEGER DEFAULT 0, hide INTEGER DEFAULT 1)")
+        self.query("CREATE TABLE IF NOT EXISTS submissions(idx INTEGER PRIMARY KEY, exid INTEGER, userid TEXT, date TEXT DEFAULT CURRENT_TIMESTAMP, corr INTEGER)")
+        self.query("CREATE TABLE IF NOT EXISTS channel (idx INTEGER PRIMARY KEY, chid TEXT, title TEXT, link TEXT, post INTEGER DEFAULT 0)")
 
-    def delete_one(self, collection, query):
-        """Delete a single document matching a query."""
-        return self.db[collection].delete_one(query)
+    def query(self, arg, values=None):
+        if values == None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        self.conn.commit()
 
-    def count_documents(self, collection, query={}):
-        """Count the number of documents matching a query."""
-        return self.db[collection].count_documents(query)
+    def fetchone(self, arg, values=None):
+        if values == None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchone()
 
-    def create_index(self, collection, field, unique=False):
-        """Create an index on a field."""
-        return self.db[collection].create_index([(field, 1)], unique=unique)
+    def fetchall(self, arg, values=None):
+        if values == None:
+            self.cur.execute(arg)
+        else:
+            self.cur.execute(arg, values)
+        return self.cur.fetchall()
 
     def __del__(self):
-        self.client.close()
+        self.conn.close()
+
+
+'''
+users: idx, userid, fullname, username, regdate, banned
+exams: idx, code, title, about, num_questions, correct, running
+submissions: idx, exid, userid, date, corr
+channel: idx, chid, title, username
+'''
