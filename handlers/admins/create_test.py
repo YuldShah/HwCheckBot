a2ca83f79c11da604ef6a5d7d8d098aebe9a46e9
YuldShah <notyuldshah@gmail.com>
2025-02-10 02:25:31 +0500

    stupid msg

--- handlers/admins/create_test.py
+++ handlers/admins/create_test.py
@@ -34,43 +34,43 @@ async def create_test(message: types.Message, state: FSMContext):
 @test.message(creates.title)
 async def get_title(message: types.Message, state: FSMContext):
     await state.update_data(title=message.text)
-    await message.answer(f"{get_text(state)}\nPlease, send the description.", reply_markup=skip_desc)
+    await message.answer(f"{await get_text(state)}\nPlease, send the description.", reply_markup=skip_desc)
     await state.set_state(creates.about)
 
 @test.message(creates.about, F.text == dict.skip)
 async def get_about(message: types.Message, state: FSMContext):
-    await message.answer(f"{get_text(state)}\nPlease, send the instructions.", reply_markup=skip_desc)
+    await message.answer(f"{await get_text(state)}\nPlease, send the instructions.", reply_markup=skip_desc)
     await state.set_state(creates.instructions)
 
 @test.message(creates.about, F.text==dict.back)
 async def back_to_title(message: types.Message, state: FSMContext):
-    await message.answer(f"{get_text(state)}\nPlease, send the title.", reply_markup=main_key)
+    await message.answer(f"{await get_text(state)}\nPlease, send the title.", reply_markup=main_key)
     await state.set_state(creates.title)
 
 @test.message(creates.about)
 async def get_about(message: types.Message, state: FSMContext):
     await state.update_data(about=message.text)
-    await message.answer(f"{get_text(state)}\nPlease, send the instructions.", reply_markup=skip_desc)
+    await message.answer(f"{await get_text(state)}\nPlease, send the instructions.", reply_markup=skip_desc)
     await state.set_state(creates.instructions)
 
 @test.message(creates.instructions, F.text == dict.skip)
 async def get_instructions(message: types.Message, state: FSMContext):
-    await message.answer(f"{get_text(state)}\nPlease, send the number of questions.", reply_markup=back_key)
+    await message.answer(f"{await get_text(state)}\nPlease, send the number of questions.", reply_markup=back_key)
     await state.set_state(creates.number)
 
 @test.message(creates.instructions, F.text == dict.back)
 async def back_to_about(message: types.Message, state: FSMContext):
-    await message.answer(f"{get_text(state)}\nPlease, send the description.", reply_markup=skip_desc)
+    await message.answer(f"{await get_text(state)}\nPlease, send the description.", reply_markup=skip_desc)
     await state.set_state(creates.about)
 
 @test.message(creates.instructions)
 async def get_instructions(message: types.Message, state: FSMContext):
     await state.update_data(instructions=message.text)
-    await message.answer(f"{get_text(state)}\nPlease, send the number of questions.", reply_markup=back_key)
+    await message.answer(f"{await get_text(state)}\nPlease, send the number of questions.", reply_markup=back_key)
     await state.set_state(creates.number)
 
 @test.message(creates.number)
 async def get_number(message: types.Message, state: FSMContext):
     await state.update_data(numquest=message.text)
-    await message.answer(f"{get_text(state)}\nPlease, send the start date in one of the following formats:\n{html.code(f"DD MM YYYY HH MM\nHH MM")} - for today", reply_markup=back_key)
+    await message.answer(f"{await get_text(state)}\nPlease, send the start date in one of the following formats:\n{html.code(f"DD MM YYYY HH MM\nHH MM")} - for today", reply_markup=back_key)
     await state.set_state(creates.sdate)
